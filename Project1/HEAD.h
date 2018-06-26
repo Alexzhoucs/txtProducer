9143884d5914b99a12b199f4041ce947f75f0a4b
Alex Zhou <alexzhoucs@gmail.com>
2018-06-26 22:01:31 +0800

    用于生成下载光明日报直播视频所用链接

---
+++ Project1/HEAD.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
---
+++ Project1/main.cpp
@@ -0,0 +1,44 @@
+#include "HEAD.h"
+
+void main()
+{
+	int i, m, n = 100;
+	string a = "http://wscdn.alhls.xiaoka.tv/2018621/300/664/8UEl_fY0-Y5NX2Ac/";
+	string b = ".ts";
+	string c = "URL.txt";
+
+	/*
+	cout << "input the URL									";
+	cin >> a;
+	cout << endl << "input the part two						";
+	cin >> b;
+	cout << endl << "input the location and the text name	";
+	cin >> c;
+	*/
+	cout << endl << "input the begin number					";
+	cin >> m;
+	cout << endl << "input the end number					";
+	cin >> n;
+	
+	cout << endl;
+	
+	fstream txt;
+	txt.open(c, ios::out | ios::trunc);
+	
+	for (i = m-1; i < n; i++)
+	{
+		txt << a << i << b << endl;
+		cout << a << i << b << endl;	
+	}
+		
+	
+	txt.close();
+	
+	getchar();
+
+
+
+}
+
+
+
